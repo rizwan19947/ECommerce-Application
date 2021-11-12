9eeec8c2f143a17946b78567da1101c1af8c2ae6
rizwan19947 <expectp@gmail.com>
2021-11-12 19:05:55 +0500

    Finished Product Detail Fragment

--- app/src/main/java/com/example/ecommerceapplication/views/ProductDetailFragment.kt
+++ app/src/main/java/com/example/ecommerceapplication/views/ProductDetailFragment.kt
@@ -1,5 +1,8 @@
 package com.example.ecommerceapplication.views
 
+import android.app.Activity
+import android.content.Context
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -9,6 +12,7 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.DataBindingUtil.*
 import androidx.fragment.app.findFragment
+import com.bumptech.glide.Glide
 import com.example.ecommerceapplication.R
 import com.example.ecommerceapplication.adapters.StaggeredRecyclerViewAdapter
 import com.example.ecommerceapplication.databinding.FragmentMarketBinding
@@ -42,18 +46,13 @@ class ProductDetailFragment : Fragment(), StaggeredRecyclerViewAdapter.Staggered
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        assignProductDetails()
 
 
 
 
 
 
-        binding.nameWidget.setText(product.title)
-        binding.priceWidget.setText(product.price.toBigDecimal().toString())
-        binding.descriptionWidget.setText(product.description)
-        binding.categoryWidget.setText(product.category)
-        binding.ratingWidget.setText(product.rate.toString())
-        binding.ratingCountWidget.setText((product.count.toString()))
 
 
 
@@ -65,5 +64,26 @@ class ProductDetailFragment : Fragment(), StaggeredRecyclerViewAdapter.Staggered
 
     }
 
+    private fun assignProductDetails(){
 
+
+        //Images
+
+        Glide.with(requireContext())
+                .load(Uri.parse(product.image))
+                .into(binding.imageviewWidget)
+
+
+
+
+
+        binding.nameWidget.setText("Product Name:\n\n "+ product.title)
+        binding.priceWidget.setText("\nPrice: $"+ product.price.toBigDecimal().toString())
+        binding.descriptionWidget.setText("\nDescription:\n\n"+ product.description)
+        binding.categoryWidget.setText("\nCategory: "+ product.category)
+        binding.ratingWidget.setText("\nRating: " + product.rate.toString() + "/5")
+        binding.ratingCountWidget.setText("\nNumber of reviews: " + (product.count.toString()))
+
+
+    }
 }
